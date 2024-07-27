4960c717e37ee0d3fc65e208c5509cda343839c9
queue-miscreant <cubebert549@gmail.com>
2024-07-27 08:26:16 -0500

    rewrite DColor class to be more imperative, add type aliases

--- dcolor/__init__.py
+++ dcolor/__init__.py
@@ -1,2 +1,6 @@
-from .dcolor import DColor
-from .color_maps import magnitude_oscillating, raw_magnitude_oscillating, green_magnitude
+from .dcolor import plot
+from .color_maps import (
+    magnitude_oscillating,
+    raw_magnitude_oscillating,
+    green_magnitude,
+)
