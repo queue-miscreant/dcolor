45e0c5c415bb359711fe51bf18241f1c65a5b9b0
queue-miscreant <cubebert549@gmail.com>
2024-07-27 13:04:18 -0500

    rewrite example script

--- examples/hsvcolorExample.py
+++
@@ -1,5 +0,0 @@
-from dcolor import DColor, raw_magnitude_oscillating
-import example
-
-dc = DColor()
-example.example(dc, color_map=raw_magnitude_oscillating)
--- examples/rgbcolorExampleExpand.py
+++
@@ -1,7 +0,0 @@
-from functools import partial
-from dcolor import DColor, green_magnitude
-
-dc = DColor()
-dc.plot(lambda z: z**2 + 4 + 4j, title="z**2+4+4j", color_map=partial(green_magnitude, expand=8))
-dc.plot(lambda z: z**8 + 4 + 4j, title="z**8+4+4j expand 8", color_map=partial(green_magnitude, expand=8))
-dc.plot(lambda z: z**16 + 4 + 4j, title="z**16+4+4j expand 16", color_map=partial(green_magnitude, expand=16))
